52d4c9a05cf576646c10f802f2b34faa62c32db3
Dasantha Edirisinghe <114149683+Tharindu-Dasantha@users.noreply.github.com>
2024-02-05 21:31:13 +0530

    settings were changed

---
+++ GUI.py
@@ -0,0 +1,27 @@
+    import tkinter as tk
+    import customtkinter as ctk
+
+    # Import the functionalities
+
+
+
+    # Create the main window
+    def GUI():
+        window = tk.Tk()
+        window.title("Password Generator")
+        
+        # Getting the password length
+        def passwordLength():
+            lengthLabel = ctk.CTkLabel(master=window, text="Password length: ", fg_color=)
+        
+        window.mainloop()
+        
+
+        # Creating a main class to run the application indivdualy
+    def main():
+        GUI()
+        
+
+    if __name__ =="__main__":
+        main()
+
