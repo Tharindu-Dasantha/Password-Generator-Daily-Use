2ecafb06bbe4fe0e53c2c65c10c95c0d144865a6
Dasantha Edirisinghe <114149683+Tharindu-Dasantha@users.noreply.github.com>
2024-02-15 22:20:32 +0530

    updated GUI.py

--- GUI.py
+++ GUI.py
@@ -1,27 +1,36 @@
-    import tkinter as tk
-    import customtkinter as ctk
-
-    # Import the functionalities
+import customtkinter as ctk
 
 
+# Create the main window
+def GUI():
+    window = ctk.CTk()
+    window.title("Password Generator")
+    window.geometry("800x600")
+    ctk.set_appearance_mode('dark')
 
-    # Create the main window
-    def GUI():
-        window = tk.Tk()
-        window.title("Password Generator")
-        
-        # Getting the password length
-        def passwordLength():
-            lengthLabel = ctk.CTkLabel(master=window, text="Password length: ", fg_color=)
-        
-        window.mainloop()
-        
 
-        # Creating a main class to run the application indivdualy
-    def main():
-        GUI()
-        
+    # Adding the components
+    # Title frame
+    Titleframe = ctk.CTkFrame(window, width=600, height=100, fg_color="transparent")
+    TitleLable = ctk.CTkLabel(Titleframe, text="Password Generator", font=("Arial", 24), justify="center", pady=20)
+    TitleLable.pack()
+    Titleframe.pack()
+    # input frame
+    Inputframe = ctk.CTkFrame(window, width=600, height=300)
+    Inputframe.pack()
+    # checkbox frame
+    
+    
 
-    if __name__ =="__main__":
-        main()
+    
+    window.mainloop()
+    
+
+    # Creating a main class to run the application indivdualy
+def main():
+    GUI()
+    
+
+if __name__ =="__main__":
+    main()
 
